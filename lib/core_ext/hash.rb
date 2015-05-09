33fa28a53c4373e301ab9b04f4126d43a0376714
Taufiq Muhammadi <waruboy@yahoo.com>
2015-05-09 20:56:37 +0700

    custom xml method for Hash

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -3,12 +3,13 @@ class Hash
     puts "custom beibih"
   end
 
-  def to_xml(options = {})
+  def to_tau_xml(options = {})
     require 'active_support/builder' unless defined?(Builder)
 
     options = options.dup
     options[:indent]  ||= 2
     options[:root]    ||= 'hash'
+    options[:tags_label] ||= 'tags'
     options[:builder] ||= Builder::XmlMarkup.new(indent: options[:indent])
 
     builder = options[:builder]
@@ -17,7 +18,17 @@ class Hash
     root = ActiveSupport::XmlMini.rename_key(options[:root].to_s, options)
 
     builder.tag!(root) do
-      each { |key, value| ActiveSupport::XmlMini.to_tag(key, value, options) }
+      each do |key, value|
+        if key == options[:tags_label]
+          builder.tags do |tag_nodes|
+            value.each do |key2, value2|
+              tag_nodes.tag(value2, "attribute" => key2)
+            end
+          end 
+        else 
+          ActiveSupport::XmlMini.to_tag(key, value, options)
+        end
+      end
       yield builder if block_given?
     end
   end
---
+++ lib/example.rb
@@ -0,0 +1,2 @@
+h = {wa: "a", "tags" => {"Shipping Method" => "Fedex"}}
+puts h.to_tau_xml
